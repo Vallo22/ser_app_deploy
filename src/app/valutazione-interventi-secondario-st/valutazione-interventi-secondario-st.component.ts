28e2f19b951ee6053e0b4080a0ee69f5dfe88f9e
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-05-26 15:12:18 +0200

    fix

--- src/app/valutazione-interventi-secondario-st/valutazione-interventi-secondario-st.component.ts
+++ src/app/valutazione-interventi-secondario-st/valutazione-interventi-secondario-st.component.ts
@@ -178,7 +178,7 @@ export class ValutazioneInterventiSecondarioStComponent implements OnInit {
     })
     this.punteggioDiVul = this.punteggio - this.deltaPunteggioFinale
     this.punteggioPassaggioClasseAggiornato = this.punteggioPassaggioClasse
-    if(this.punteggioDiVul < this.soglia) {
+    if(this.punteggioDiVul - this.sommaPacchettoInterventi < this.soglia) {
       this.vulClassAggiornata = this.vulClass - 1
     } else {
       this.vulClassAggiornata = this.vulClass
@@ -280,6 +280,7 @@ export class ValutazioneInterventiSecondarioStComponent implements OnInit {
   }
 
   trasferisciClasseDiRischio(){
+    this.punteggioDiVul = this.punteggio - this.sommaPacchettoInterventi
     this.router.navigate(['/riepilogo-costi-st'], {
       state: {
         risk: this.risk,
